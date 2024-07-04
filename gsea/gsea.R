cf835187f9640462553e067ff6d6a98ebd1b2a0b
Jean Silva de Souza Resende <jean.souza@edu.unipar.br>
2024-07-04 15:33:02 -0300

    gsea

---
+++ gsea/gsea.R
@@ -0,0 +1,87 @@
+################################################################################
+
+
+
+
+
+################################################################################
+# Instalar e carregar pacotes necessários
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+
+#BiocManager::install("clusterProfiler")
+#BiocManager::install("org.Dr.eg.db")
+#BiocManager::install("msigdbr")
+
+library(clusterProfiler)
+library(org.Dr.eg.db)
+library(msigdbr)
+library(dplyr)
+
+# Carregar os dados de genes diferencialmente expressos
+# Suponha que seu dataframe se chama 'df' com colunas 'gene' e 'log2FoldChange'
+# Ordene os genes pela estatística que deseja usar (e.g., log2FoldChange)
+
+df <- read.csv("ahCutUncut0_allSamples_res05_sig_fc0.csv")
+df <- df[order(df$log2FoldChange, decreasing = TRUE), ]
+
+
+#df <- genes[order(genes$log2FoldChange, decreasing = TRUE), ]
+
+# Transformar os genes em uma lista nomeada
+gene_list <- df$log2FoldChange
+names(gene_list) <- df$X
+
+# Obter conjuntos de genes para Danio rerio (por exemplo, C5 collection)
+msigdb_genes <- msigdbr(species = "Danio rerio")
+
+# Realizar a análise GSEA
+gsea_result <- GSEA(
+  geneList = gene_list,
+  TERM2GENE = msigdb_genes %>% select(gs_name, ensembl_gene),
+  pvalueCutoff = 0.05,
+  verbose = FALSE
+)
+
+# Visualizar resultados
+gsea_result_df <- as.data.frame(gsea_result)
+print(head(gsea_result_df))
+
+write.csv(gsea_result_df, file = "gsea_ahCutUncut0_allSamples_rees05_sig_fc0.csv")
+
+# Plotar resultados significativos
+library(enrichplot)
+dotplot(gsea_result) + ggtitle("GSEA Dotplot")
+
+# Exibir as 15 principais categorias com um ajuste de cor para o p-value ajustado
+dotplot(gsea_result, showCategory = 15, color = "p.adjust") + ggtitle("Top 15 GSEA Dotplot")
+
+gseaplot2(gsea_result, geneSetID = 1:28, title = "GSEA Plot for Top Pathway")
+
+
+
+
+
+cnetplot(gsea_result, categorySize = "pvalue", foldChange = gene_list)
+
+heatplot(gsea_result, foldChange = gene_list)
+
+ridgeplot(gsea_result)
+
+upsetplot(gsea_result)
+
+gseatable(gsea_result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
